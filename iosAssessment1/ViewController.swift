5d55198f6959da9efda1dabfbaac4b20c1ca6e1c
Kirk Brown <kirkbrown@macbook-pro-97.local>
2018-10-23 10:14:53 -0500

    Initial Commit

---
+++ iosAssessment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosAssessment1
+//
+//  Created by Kirk Brown on 10/23/18.
+//  Copyright © 2018 Kirk Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
